6fd8c734e631ac3096e81bbbe30c156652aec7bc
Joseph Putz <josephputz89@gmail.com>
2023-06-30 19:56:36 -0500

    finish

--- Ch5 - Internet Data/inetdata_start.py
+++ Ch5 - Internet Data/inetdata_start.py
@@ -3,8 +3,16 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+import urllib.request
+
 def main():
-    pass # this is a placeholder, do-nothing statement
+    weburl = urllib.request.urlopen("http://www.google.com")
+    print("result code:", weburl.getcode())
+    # result code (200 if it's working)
+    data = weburl.read()
+    print(data)
+    # the html code for google homepage
+    
 
 if __name__ == "__main__":
     main()
